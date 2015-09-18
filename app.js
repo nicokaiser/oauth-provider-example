4cfc03abe407ada4284e980bbdd49b94304c1a76
Nico Kaiser <nico.kaiser@boerse-go.de>
2015-09-18 15:09:33 +0200

    Fix implicit flow, add refresh_token

--- app.js
+++ app.js
@@ -27,8 +27,7 @@ app.use(session({
     saveUninitialized: true,
     store: new FileStore({
         path: './sessions',
-        ttl: 1 * 24 * 3600,
-        reapInterval: 1 * 60
+        ttl: 1 * 24 * 3600
     }),
     cookie: {
         maxAge: 1 * 24 * 3600 * 1000
